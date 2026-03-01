4cceb5ba515052cccd1115053e036dacfb18882f
WaleedJumaa98 <wwaleed.jumaa@gmail.com>
2026-03-01 01:24:28 +0100

    add groups

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         log_state()
@@ -19,12 +24,13 @@ def main():
 
         screen.fill("black")
 
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
-
+        
+        updatable.update(dt)
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
